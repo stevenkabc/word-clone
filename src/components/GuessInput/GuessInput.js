4016ca9286e523bf4a140029292172145a3ecdd4
steven <steven.bitbucket@redwood6.com>
2023-02-09 11:27:59 -0500

    added gameOver state to exercise 5

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -5,6 +5,7 @@ import {checkGuess} from "../../game-helpers"
 
 
 function GuessInput({answer}) {
+  const [gameOver, setGameOver] = React.useState(false)
   const [isMatch, setIsMatch] = React.useState(false);
   const [guess, setGuess] = React.useState('');
   const [guessCount,setGuessCount] = React.useState(0);
@@ -23,14 +24,18 @@ function GuessInput({answer}) {
   function submitHandler(event) {
     event.preventDefault();
 
+    
+    if(gameOver) return;
     if (guess.length !== 5){
       window.alert('Enter exactly 5 characters')
       return
     }
     if(guess === answer){
       setIsMatch(true);
+      setGameOver(true)
     } else if (guessCount === NUM_OF_GUESSES_ALLOWED ) {
       setIsMatch(false)
+      setGameOver(true)
     }
        
      
@@ -65,6 +70,7 @@ function GuessInput({answer}) {
           minLength={5}
           maxLength={5}
           onChange={(event) => { 
+            console.log('onChange guess count is ' + guessCount)
             if( guessCount === 6) return;
             const newGuess=event.target.value.toUpperCase()
             setGuess(newGuess)
